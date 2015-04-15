d7a438c8975b656c0431dfac38dcb189f20e00d5
hiroyuki okada <ok.oakda.hiroyuki@gmail.com>
2015-04-15 23:57:02 +0900

    add ja

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+'use strict';
+
+function change_lang(obj){
+  var val = obj.options[obj.selectedIndex].value;
+  var loc = window.location;
+  var path = '';
+  if(val === 'ja') path = "/ja";
+  console.log(val);
+  console.log(path);
+  window.location = loc.protocol + '//' + loc.host + path + '/index.html';
+}
